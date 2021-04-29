bc36d8708a574d9a8b4dedc0eee3619427b0f502
w00j00ng <w00j00ng351@gmail.com>
2021-04-29 16:26:13 +0900

    Modify mydata_weekday 2

--- arcafe/views/mydata_views.py
+++ arcafe/views/mydata_views.py
@@ -47,7 +47,7 @@ def bydate():
 
 @bp.route('/byweek/')
 def byweek():
-    query_mydata = db.engine.execute(f"SELECT SUM(totalWorkingTime) / SUM(operationTime) * 100 / ROWNUMBER"
+    query_mydata = db.engine.execute(f"SELECT SUM(totalWorkingTime) / SUM(operationTime) * 100"
                                      f'     , SUM(blinkCount) / SUM(totalWorkingTime) * 60'
                                      f'     , SUM(warningCount)/ SUM(totalWorkingTime) * 60'
                                      f'     , SUM(alertCount)/ SUM(totalWorkingTime) * 60'
