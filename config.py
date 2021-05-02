0665f66469925f8056358bf275bded3d613e71a7
w00j00ng <w00j00ng351@gmail.com>
2021-05-02 23:33:44 +0900

    Change to public

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 SECRET_KEY = 'dev'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://hankyungedudb002:gksrud70292!@my8001.gabiadb.com/hankyung002'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if __name__ == '__main__':
