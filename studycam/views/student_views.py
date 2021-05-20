f89c2cadbfc8067195f2c1af31ca6d19691a892f
w00j00ng <magoswj@gmail.com>
2021-05-20 15:02:35 +0900

    Show Total Data To student

--- studycam/views/student_views.py
+++ studycam/views/student_views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask import session, g
 from studycam.models import User
 from studycam import db
+from studycam.models import StudyLog
 import pandas as pd
+from sqlalchemy import and_
 
 
 bp = Blueprint('student', __name__, url_prefix='/student/')
@@ -19,4 +21,25 @@ def load_logged_in_user():
 
 @bp.route('/')
 def index():
-    return render_template('student/index.html')
+    return redirect(url_for('student.total'))
+
+
+@bp.route('/total/')
+def total():
+    data = db.engine.execute(f"SELECT   lecture_id "
+                             f"       , lecture_part "
+                             f"       , rate_posture "
+                             f"       , rate_concentrate "
+                             f"       , COUNT(*)"
+                             f"FROM     study_log "
+                             f"WHERE    student_id = {session.get('user_id')} "
+                             f"GROUP BY lecture_id "
+                             f"       , lecture_part "
+                             f"ORDER BY id ")
+    data_dict = {}
+    for row in data:
+        if row[0] not in data_dict:
+            data_dict[row[0]] = {}
+        data_dict[row[0]][row[1]] = {'rate_posture': row[2], 'rate_concentrate': row[3], 'count': row[4]}
+    print(data_dict)
+    return render_template('student/total.html', data=data_dict)
