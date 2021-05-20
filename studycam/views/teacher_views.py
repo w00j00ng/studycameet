3cc8a080c7dfd6822a1d1a4495fe9997e3f8cd07
w00j00ng <magoswj@gmail.com>
2021-05-20 11:41:37 +0900

    Pring group by data in teacher page

--- studycam/views/teacher_views.py
+++ studycam/views/teacher_views.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template
 from flask import session, g
 from studycam.models import User
 from studycam import db
+from studycam.models import StudyLog
 import pandas as pd
+from sqlalchemy import and_
 
 
 bp = Blueprint('teacher', __name__, url_prefix='/teacher/')
@@ -20,3 +22,20 @@ def load_logged_in_user():
 @bp.route('/')
 def index():
     return render_template('teacher/index.html')
+
+
+@bp.route('/total/')
+def total():
+    data = db.engine.execute(f"SELECT   lecture_id "
+                             f"       , lecture_part "
+                             f"       , AVG(rate_posture) "
+                             f"       , AVG(rate_concentrate) "
+                             f"       , COUNT(*)"
+                             f"FROM     study_log "
+                             f"WHERE    teacher_id = {session.get('user_id')} "
+                             f"GROUP BY lecture_id "
+                             f"       , lecture_part")
+
+    for row in data:
+        print(row)
+    return render_template('teacher/total.html')
