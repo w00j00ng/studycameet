eb16fcaea535d47bc9f1683f1885ae0000a62e88
w00j00ng <magoswj@gmail.com>
2021-05-23 17:33:52 +0900

    Show emotion information to teacher

--- studycam/views/teacher_views.py
+++ studycam/views/teacher_views.py
@@ -4,6 +4,7 @@ from studycam.models import User
 from studycam import db
 from studycam.models import StudyLog
 from sqlalchemy import and_
+import heapq
 
 
 bp = Blueprint('teacher', __name__, url_prefix='/teacher/')
@@ -29,6 +30,12 @@ def total():
                              f"       , lecture_part "
                              f"       , AVG(rate_posture) "
                              f"       , AVG(rate_concentrate) "
+                             f"       , AVG(rate_concentrate) "
+                             f"       , AVG(rate_angry) "
+                             f"       , AVG(rate_disgust) "
+                             f"       , AVG(rate_fear) "
+                             f"       , AVG(rate_happy) "
+                             f"       , AVG(rate_sad) "
                              f"       , COUNT(*) "
                              f"FROM     study_log "
                              f"WHERE    teacher_id = {session.get('user_id')} "
@@ -41,7 +48,16 @@ def total():
     for row in data:
         if row[0] not in data_dict:
             data_dict[row[0]] = {}
-        data_dict[row[0]][row[1]] = {'rate_posture': row[2], 'rate_concentrate': row[3], 'count': row[4]}
+        emotion_list = [row[5], row[6], row[7], row[8], row[9]]
+        emotion_label = ['스트레스', '우울', '불안', '행복', '슬픔']
+        emotion_rank = heapq.nlargest(2, range(len(emotion_list)), key=emotion_list.__getitem__)
+        data_dict[row[0]][row[1]] = {
+            'rate_posture': row[2],
+            'rate_concentrate': row[3],
+            'max_emotion': emotion_label[emotion_rank[0]],
+            'second_emotion': emotion_label[emotion_rank[1]],
+            'count': row[10]
+        }
         rownum += 1
     if rownum == 0:
         return render_template('teacher/empty.html')
