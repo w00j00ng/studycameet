93207a656d955e9b34b4ffe5b9ae2af443c37f97
w00j00ng <magoswj@gmail.com>
2021-05-24 10:19:15 +0900

    Revert "Refactoring variable names 2"
    This reverts commit ab3a8f195d7d7aa73083b6067bcc749596256a1f.

--- studycam/views/student_views.py
+++ studycam/views/student_views.py
@@ -46,8 +46,7 @@ def by_lecture():
             'grade_posture': get_grade(row[2]),
             'rate_concentrate': row[3],
             'grade_concentrate': get_grade(row[3]),
-            'count': row[4]
-        }
+            'count': row[4]}
         posture_sum += row[2]
         concentrate_sum += row[3]
         row_num += 1
